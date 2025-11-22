0ca6449a5ca463e37d2a15fcf47938482b9828ee
edissonchamorroc <john.chamorro@udea.edu.co>
2025-11-22 18:50:48 -0500

    Se suben video y se juega con paremetrizacion para trabajar con una segmentaci[on adecuada. Falta mejorar esa parte

--- Tarea_3/detect.py
+++ Tarea_3/detect.py
@@ -18,6 +18,8 @@ red_low2  = np.array([170, 150, 80])
 red_high2 = np.array([180, 255, 255])
 
 
+white_low  = np.array([0, 0, 200])
+white_high = np.array([180, 40, 255])
 # ======================
 #   DETECTOR DE FORMAS
 # ======================
@@ -25,14 +27,14 @@ def detectar_forma(contour):
     peri = cv2.arcLength(contour, True)
     approx = cv2.approxPolyDP(contour, 0.03 * peri, True)
 
-    if len(approx) > 8:
+    if len(approx) >8:
         return "circulo"
     return "otro"
 
 # ======================
 #   VIDEO
 # ======================
-cap = cv2.VideoCapture("dataset/test/videos/video1.mp4")
+cap = cv2.VideoCapture("dataset/test/videos/video3.mp4")
 
 while True:
     ret, frame = cap.read()
@@ -42,15 +44,17 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # ---- MÁSCARAS ----
-
+     # Rojo
     mask_r1 = cv2.inRange(hsv, red_low1, red_high1)
     mask_r2 = cv2.inRange(hsv, red_low2, red_high2)
     mask_red = cv2.bitwise_or(mask_r1, mask_r2)
-  
 
-    mask_total = mask_red
+    # Blanco
+    mask_white = cv2.inRange(hsv, white_low, white_high)
+
+    # Señales reglamentarias → rojo borde + blanco centro
+    mask_total = cv2.bitwise_or(mask_red, mask_white)
    
-
     # ---- LIMPIEZA ----
     kernel = np.ones((5,5), np.uint8)
     mask_clean = cv2.morphologyEx(mask_total, cv2.MORPH_OPEN, kernel)
@@ -61,20 +65,17 @@ while True:
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area < 500:
+        if area < 100:
             continue
 
         forma = detectar_forma(cnt)
-        if forma not in ["circulo", "rombo"]:
+        if forma not in ["circulo"]:
             continue
 
-        # ---- ROI EN MASCARA ----
         x, y, w, h = cv2.boundingRect(cnt)
 
-        # ---- ROI EN IMAGEN ORIGINAL (IMPORTANTÍSIMO) ----
-        roi_color = frame[y:y+h, x:x+w]
+        roi_color = frame[y:y+100, x:x+100]
 
-        # evitar errores si ROI sale muy pequeño
         if roi_color.size == 0:
             continue
 
@@ -89,21 +90,21 @@ while True:
                 conf = float(box.conf[0])
                 nombre = model.names[cls]
 
-                # Dibujar bbox en frame completo
+                # <<< AQUI IMPRIMIMOS >>>
+                print(f"Señal detectada: {nombre} — Confianza: {conf:.2f}")
+
+                # Dibujar bbox
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
                 cv2.putText(frame, f"{nombre} {conf:.2f}", (x, y-10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
 
-        # Mostrar señal segmentada (ROI procesada)
         cv2.imshow("Señal segmentada", roi_color)
 
-    # Ventanas principales
     cv2.imshow("Frame original", frame)
     cv2.imshow("Mascara segmentacion", mask_clean)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 
 cap.release()
 cv2.destroyAllWindows()
